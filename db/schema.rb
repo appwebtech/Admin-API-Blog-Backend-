074cc28920831f10f0ff6dbd8e1c04a19a183d52
appwebtech <josembi@gmail.com>
2016-11-01 18:47:32 +0100

    Generate Settings Model & Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101171146) do
+ActiveRecord::Schema.define(version: 20161101174338) do
 
   create_table "moderators", force: :cascade do |t|
     t.string   "fullname"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 20161101171146) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "settings", force: :cascade do |t|
+    t.string   "site_name"
+    t.integer  "post_per_page"
+    t.boolean  "under_maintenance"
+    t.boolean  "tag_visibility"
+    t.boolean  "prevent_commenting"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
 end
