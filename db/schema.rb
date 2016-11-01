80f2d3273912e8b4af7f24b0f1d03b1748d35d8f
appwebtech <josembi@gmail.com>
2016-11-01 19:21:58 +0100

    Generate Models for Posts, Comments & Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101174338) do
+ActiveRecord::Schema.define(version: 20161101182040) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "message"
+    t.boolean  "status"
+    t.integer  "post_id"
+    t.integer  "visitor_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["visitor_id"], name: "index_comments_on_visitor_id"
+  end
 
   create_table "moderators", force: :cascade do |t|
     t.string   "fullname"
@@ -18,6 +29,16 @@ ActiveRecord::Schema.define(version: 20161101174338) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.boolean  "publish"
+    t.integer  "moderator_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["moderator_id"], name: "index_posts_on_moderator_id"
   end
 
   create_table "settings", force: :cascade do |t|
