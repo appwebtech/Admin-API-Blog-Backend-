14c0642a4e87ea9b7d13ed31903a52daee564b92
appwebtech <josembi@gmail.com>
2016-11-01 18:29:04 +0100

    Moderators Model, Migrations & Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+moderator = Moderator.create(
+	fullname: "Joseph Mwania",
+	username: "josembi@gmail.com",
+	password: "foobar")
