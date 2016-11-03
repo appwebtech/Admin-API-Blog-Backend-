769cc8094ea60b74b2afef38261bc7d0ced1dff7
appwebtech <josembi@gmail.com>
2016-11-03 21:26:42 +0100

    Functionality for Edit Moderator Page

--- app/controllers/admin/moderators_controller.rb
+++ app/controllers/admin/moderators_controller.rb
@@ -8,6 +8,19 @@ class	Admin::ModeratorsController < ApplicationController
 	end
 
 	def update
-		
+		@moderator = Moderator.find(params[:id])
+		if @moderator.update(moderator_params)
+			redirect_to :back
+		else
+			render 'edit'
+		end
+	end
+
+	# Strong Parameters will go below here. 
+	private
+
+	def moderator_params
+		params.require(:moderator).permit(:id, :fullname, :username, :password)
+	
 	end
 end
