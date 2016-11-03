f6a3b0a50336f4397584f0ad2f0a5fc8338cde87
appwebtech <josembi@gmail.com>
2016-11-03 23:40:13 +0100

    CRUDify Controllers and Securitize

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+	get '/login' => 'admin/sessions#new'
   namespace :admin do 
+  	resources :sessions, only: [:new, :create, :destroy]
   	resources :moderators, only: [:index, :edit, :update] # Dont want moderator to delete himself. Se he can only see index.
   end
 end
