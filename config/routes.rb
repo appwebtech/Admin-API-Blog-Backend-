9361ebdcf2181a3200c46e9d07abb450e1f718e4
appwebtech <josembi@gmail.com>
2016-11-03 21:06:40 +0100

    Edit Form, Routes and Controller CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   namespace :admin do 
-  	resources :moderators, only: [:index] # Dont want moderator to delete himself. Se he can only see index.
+  	resources :moderators, only: [:index, :edit, :update] # Dont want moderator to delete himself. Se he can only see index.
   end
 end
