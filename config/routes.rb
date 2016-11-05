7c5c6738b213fdde9b576878d5228a7dadf3b467
appwebtech <josembi@gmail.com>
2016-11-05 09:30:09 +0100

    Do Housekeeping Post Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,5 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'posts/index'
-  end
 
-  namespace :admin do
-    get 'posts/new'
-  end
-
-  namespace :admin do
-    get 'posts/create'
-  end
-
-  namespace :admin do
-    get 'posts/edit'
-  end
-
-  namespace :admin do
-    get 'posts/update'
-  end
-
-  namespace :admin do
-    get 'posts/show'
-  end
-
-  namespace :admin do
-    get 'posts/destroy'
-  end
 
 	get '/login' => 'admin/sessions#new'
 	get '/logout' => 'admin/sessions#destroy'
@@ -34,5 +8,6 @@ Rails.application.routes.draw do
   namespace :admin do 
   	resources :sessions, only: [:new, :create, :destroy]
   	resources :moderators, only: [:index, :edit, :update] # Dont want moderator to delete himself. Se he can only see index.
+    resources :posts
   end
 end
