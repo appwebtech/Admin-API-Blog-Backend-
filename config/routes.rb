3c636e880ea2bbfd0b9b3bc096d66effbde8636d
appwebtech <josembi@gmail.com>
2016-11-05 09:23:19 +0100

    Post Controller with ALL RESTful CRUD Methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'posts/index'
+  end
+
+  namespace :admin do
+    get 'posts/new'
+  end
+
+  namespace :admin do
+    get 'posts/create'
+  end
+
+  namespace :admin do
+    get 'posts/edit'
+  end
+
+  namespace :admin do
+    get 'posts/update'
+  end
+
+  namespace :admin do
+    get 'posts/show'
+  end
+
+  namespace :admin do
+    get 'posts/destroy'
+  end
+
 	get '/login' => 'admin/sessions#new'
 	get '/logout' => 'admin/sessions#destroy'
 
