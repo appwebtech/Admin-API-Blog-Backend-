cbf6d26f08987654c9425fa498d4e641ae1e0324
appwebtech <josembi@gmail.com>
2016-11-05 08:14:50 +0100

    Drop DB, re-seed && Moderator Sessio

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,10 +1,13 @@
 class Admin::SessionsController < Admin::ApplicationController
 	before_action :authorize, except: [:new, :create]
-	
+
   def new
+
   end
 
   def create
+  	@moderator = Moderator.find_by(username: params[:username]).authenticate(params[:password])
+  	session[:current_moderator_id] = @moderator.id
   end
 
   def destroy
