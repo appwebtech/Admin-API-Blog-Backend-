d5f229fdb02e8a04bc9cb211ae943e74a6907e6b
appwebtech <josembi@gmail.com>
2016-11-05 11:17:44 +0100

    Searching Posts Functionality

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,11 @@
 class Admin::PostsController < Admin::ApplicationController
   def index
+    if params[:search].present?
+      @posts = Post.where("title LIKE ? OR content LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%").page params[:page]
+    else 
     @posts = Post.all.order(id: :desc).page params[:page]  # kaminari to paginate
   end
+end
 
   def new
   end
