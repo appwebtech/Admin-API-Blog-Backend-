9c16e290145f8c4d659f47680fab672ff436438e
appwebtech <josembi@gmail.com>
2016-11-05 09:04:53 +0100

    Finish Moderator Login / Logout Design

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 	get '/login' => 'admin/sessions#new'
+	get '/logout' => 'admin/sessions#destroy'
+
+
   namespace :admin do 
   	resources :sessions, only: [:new, :create, :destroy]
   	resources :moderators, only: [:index, :edit, :update] # Dont want moderator to delete himself. Se he can only see index.
