888a0e83ba4591617b051d25650c91fbe6525c7e
appwebtech <josembi@gmail.com>
2016-11-05 09:19:05 +0100

    Add Admin Controller & Inherit Shyt from AppContr::Base

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,5 +1,13 @@
-class Admin::ApplicationController < ApplicationController
+class Admin::ApplicationController < ActionController::Base
+
+	# Added the method below to prevent CSRF attacks. 
+	# Wont use null_session for API within the Josembi applications for obvious reasons.
+	protect_from_forgery with: :exception
+
+	layout 'admin'
+
 	before_action :authorize
+
 	def current_moderator
 		@moderator ||= Moderator.find(session[:current_moderator_id]) if session[:current_moderator_id]
 	end
