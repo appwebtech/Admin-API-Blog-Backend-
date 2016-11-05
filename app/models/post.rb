023448c89b17951298412ae12f7e9fc470e1dc35
appwebtech <josembi@gmail.com>
2016-11-05 15:13:32 +0100

    Write Helper Methods and Refactor

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
 	has_many :comments
 	has_many :tags, through: :post_tags
   belongs_to :moderator
+
+  def self.josembi search
+  	where("title LIKE ? OR content LIKE ?", "%#{search}%", "%#{search}%")
+  end
 end
