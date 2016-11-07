48a2a2f0f770336db065b8e526711257a3d99cf3
appwebtech <josembi@gmail.com>
2016-11-07 21:48:25 +0100

    Comments Controller & Do Routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   	resources :sessions, only: [:new, :create, :destroy]
   	resources :moderators, only: [:index, :edit, :update] # Dont want moderator to delete himself. Se he can only see index.
     resources :posts
+    resources :comments, only: [:index, :update, :destroy]
     resources :tags, except: [:index]
   end
 end
