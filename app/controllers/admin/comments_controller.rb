9fd0eeba7296160b6ff1b1d6bf1dad9252fbb43c
appwebtech <josembi@gmail.com>
2016-11-07 22:03:17 +0100

    Amazingly Un-Fucked a Deserialized Boolean and Parsed in Com' :-)

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,5 +1,6 @@
 class Admin::CommentsController < Admin::ApplicationController
   def index
+  	@comments = Comment.where(status: ActiveRecord::Type::Boolean.new.deserialize(params[:status]))
   end
 
   def update
