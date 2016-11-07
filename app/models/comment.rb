9aac67db53ca3b44fee24b7df513e742f7c1898b
appwebtech <josembi@gmail.com>
2016-11-07 23:03:09 +0100

    Refactor Code ~ Ignore Partials for Now

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :visitor
   has_many :notifications, as: :notifiable,  dependent: :destroy
+
+  # def josembi_wa_kimeu params
+  # 	joins(:visitor).where("fullname LIKE ? OR message LIKE ?", "%#{params}%", "%#{params}%")
+  # end
 end
