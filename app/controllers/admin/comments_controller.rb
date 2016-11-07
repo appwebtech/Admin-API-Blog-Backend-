df3fea8d8beab80502ec4950aa6b4b24de3b59d9
appwebtech <josembi@gmail.com>
2016-11-07 22:34:28 +0100

    Finish Search Functionality && Comment

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,9 +1,19 @@
 class Admin::CommentsController < Admin::ApplicationController
   def index
-  	@comments = Comment.where(status: ActiveRecord::Type::Boolean.new.deserialize(params[:status]))
+  	if params[:search].present?
+  		@comments = Comment.joins(:visitor).where("fullname LIKE ? OR message LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%").page params[:page]
+  	else
+  	@comments = Comment.where(status: ActiveRecord::Type::Boolean.new.deserialize(params[:status])).page params[:page]
   end
+ end
 
   def update
+  	@comment = Comment.find(params[:id])
+  	if @comment.update(status: params[:status])
+  		redirect_to :back, notice: 'Successfully Updated Comment'
+  	else
+  		redirect_to :back, notice: 'There was a Problem Updating Comment'
+  	end
   end
 
   def destroy
