a0430967dd86af4a896867c86f7e67ec7c9ad3b6
appwebtech <josembi@gmail.com>
2016-11-10 20:47:18 +0100

    Updating READ & UN-READ Messages

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -1,13 +1,22 @@
 class Admin::MessagesController < Admin::ApplicationController
   def index
+  	@messages = Message.all.order(id: :desc).page params[:page]
   end
 
   def show
   end
 
   def update
+    @message = Message.find(params[:id])
+    @message.update(status: params[:status])
+
+    redirect_to :back, notice: 'Successfullly updated message'
   end
 
   def destroy
+  	@message = Message.find(params[:id])
+  	@message = Message.destroy
+
+  	redirect_to :back, notice: 'Successfully deleted Visitor'
   end
 end
