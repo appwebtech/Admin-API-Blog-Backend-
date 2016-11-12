9fa779f87b2cf469fb01e29a07ea80613a3510e5
appwebtech <josembi@gmail.com>
2016-11-12 08:39:48 +0100

    Finish Post Client/Server Side

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
   def index
+  	if params[:tag]
+  		@posts = Post.joe_beppi_tags(params[:tag]).page(params[:page]).per(Setting.post_per_page)
+  	else
   	@posts = Post.where(publish: true).order(id: :desc).page(params[:page]).per(Setting.post_per_page)
   end
+ end
 
   def show
   end
