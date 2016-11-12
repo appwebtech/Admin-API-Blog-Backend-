cd400469890d5237cedd9a2632bd7559cfd059c9
appwebtech <josembi@gmail.com>
2016-11-12 11:49:06 +0100

    Sessions, Testing in Isolation & Services

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,7 @@ class CommentsController < ApplicationController
 	  	flash[:notice] = "Successfully Created New Comment"
 	  else
 	  	flash[:alert] = "There was a problem creating your comment"
+	  	set_visitor_sessions
 	  end
 	  	redirect_to :back
   end
@@ -16,7 +17,13 @@ class CommentsController < ApplicationController
   end
 
 	def visitor
-		VisitorJosembiService.new(visitor_comments_params).visitor
+		# Have to cache here so I wont hit the DB multiple times.
+		@visitor ||= VisitorJosembiService.new(visitor_comments_params).visitor
+	end
+
+	def set_visitor_sessions
+		session[:visitor_errors] = visitor.errors.full_messages
+		session[:visitor_params] = visitor_comments_params
 	end
 end
 
